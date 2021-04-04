31a3f5be788b3fe030ddbe894723f71ee8c16255
J/A <archer884@gmail.com>
2021-04-04 14:44:05 -0500

    Use sparse array decoder

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,146 @@ use std::{
 };
 
 use clap::Clap;
-use hashbrown::HashMap;
 
 type Code = &'static str;
 type Result<T, E = Error> = std::result::Result<T, E>;
 
-static CODE: &[&str] = &[
-    ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--",
-    "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..",
-    "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.",
-];
+mod data {
+    pub static ENCODED_SEQUENCES: &[&str] = &[
+        ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--",
+        "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--",
+        "--..", "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..",
+        "----.",
+    ];
+
+    pub static DECODING_ARRAY: &[Option<u8>] = &[
+        Some(b'0'),
+        None,
+        None,
+        None,
+        Some(b'9'),
+        None,
+        Some(b'O'),
+        None,
+        None,
+        None,
+        None,
+        None,
+        Some(b'8'),
+        None,
+        Some(b'M'),
+        None,
+        None,
+        None,
+        Some(b'Q'),
+        None,
+        None,
+        None,
+        Some(b'G'),
+        None,
+        None,
+        None,
+        Some(b'Z'),
+        None,
+        Some(b'7'),
+        None,
+        Some(b'T'),
+        None,
+        None,
+        None,
+        Some(b'Y'),
+        None,
+        None,
+        None,
+        Some(b'K'),
+        None,
+        None,
+        None,
+        Some(b'C'),
+        None,
+        None,
+        None,
+        Some(b'N'),
+        None,
+        None,
+        None,
+        Some(b'X'),
+        None,
+        None,
+        None,
+        Some(b'D'),
+        None,
+        None,
+        None,
+        Some(b'B'),
+        None,
+        Some(b'6'),
+        None,
+        None,
+        None,
+        Some(b'1'),
+        None,
+        Some(b'J'),
+        None,
+        None,
+        None,
+        Some(b'W'),
+        None,
+        None,
+        None,
+        Some(b'P'),
+        None,
+        None,
+        None,
+        Some(b'A'),
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        Some(b'R'),
+        None,
+        None,
+        None,
+        Some(b'L'),
+        None,
+        None,
+        None,
+        Some(b'E'),
+        None,
+        Some(b'2'),
+        None,
+        None,
+        None,
+        None,
+        None,
+        Some(b'U'),
+        None,
+        None,
+        None,
+        Some(b'F'),
+        None,
+        None,
+        None,
+        Some(b'I'),
+        None,
+        Some(b'3'),
+        None,
+        Some(b'V'),
+        None,
+        None,
+        None,
+        Some(b'S'),
+        None,
+        Some(b'4'),
+        None,
+        Some(b'H'),
+        None,
+        Some(b'5'),
+    ];
+}
 
 #[derive(Clap, Clone)]
 enum Opts {
@@ -41,37 +171,6 @@ impl Display for Error {
 }
 
 impl std::error::Error for Error {}
-
-struct CharacterDecoder {
-    map: HashMap<Code, char>,
-}
-
-impl CharacterDecoder {
-    fn new() -> Self {
-        // Looking back, I can't for the life of me figure
-        // why I did this in two iterations.
-        let letters = &CODE[..26];
-        let letters = letters
-            .iter()
-            .copied()
-            .zip("ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars());
-
-        let numbers = &CODE[26..];
-        let numbers = numbers.iter().copied().zip("0123456789".chars());
-
-        Self {
-            map: letters.chain(numbers).collect(),
-        }
-    }
-
-    #[inline]
-    fn decode(&self, character: &str) -> Result<char> {
-        self.map
-            .get(character)
-            .copied()
-            .ok_or_else(|| Error::Decode(character.to_string()))
-    }
-}
 
 fn main() {
     let opts = Opts::parse();
@@ -100,8 +199,7 @@ fn run(opts: &Opts) -> Result<()> {
         }
 
         Opts::Decode => {
-            let character_decoder = CharacterDecoder::new();
-            println!("{}", decode_message(message.trim(), &character_decoder)?);
+            println!("{}", decode_message(message.trim())?);
         }
     }
 
@@ -129,17 +227,17 @@ fn encode_message(message: &str) -> Result<String> {
     Ok(buf)
 }
 
-fn decode_message(message: &str, character_decoder: &CharacterDecoder) -> Result<String> {
+fn decode_message(message: &str) -> Result<String> {
     let mut buf = String::new();
     let mut words = message.split('/');
 
     if let Some(word) = words.next() {
-        decode_word_into(word, &mut buf, character_decoder)?;
+        decode_word_into(word, &mut buf)?;
     }
 
     for word in words {
         buf.push(' ');
-        decode_word_into(word, &mut buf, character_decoder)?;
+        decode_word_into(word, &mut buf)?;
     }
 
     Ok(buf)
@@ -149,28 +247,56 @@ fn decode_message(message: &str, character_decoder: &CharacterDecoder) -> Result
 fn encode_byte(u: u8) -> Result<Code> {
     static NUMERIC_RANGE: RangeInclusive<u8> = b'0'..=b'9';
     match u {
-        u if u.is_ascii_alphabetic() => Ok(CODE[(u.to_ascii_uppercase() - b'A') as usize]),
-        u if NUMERIC_RANGE.contains(&u) => Ok(CODE[(u - b'0' + 26) as usize]),
+        u if u.is_ascii_alphabetic() => {
+            Ok(data::ENCODED_SEQUENCES[(u.to_ascii_uppercase() - b'A') as usize])
+        }
+        u if NUMERIC_RANGE.contains(&u) => Ok(data::ENCODED_SEQUENCES[(u - b'0' + 26) as usize]),
         _ => Err(Error::Encode(u as char)),
     }
 }
 
-fn decode_word_into(
-    word: &str,
-    buf: &mut String,
-    character_decoder: &CharacterDecoder,
-) -> Result<()> {
+fn decode_word_into(word: &str, buf: &mut String) -> Result<()> {
     let mut characters = word.split_whitespace();
 
     if let Some(character) = characters.next() {
-        buf.push(character_decoder.decode(character)?);
+        buf.push(decode_character(character)? as char);
     }
 
     for character in characters {
-        buf.push(character_decoder.decode(character)?);
+        buf.push(decode_character(character)? as char);
     }
 
     Ok(())
+}
+
+#[inline]
+fn decode_character(character: &str) -> Result<u8> {
+    /// Correction factor to raise minimum index to zero.
+    const MAGIC_NUMBER: i32 = 62;
+
+    let idx = uncorrected_offset(character) + MAGIC_NUMBER;
+    data::DECODING_ARRAY
+        .get(idx as usize)
+        .copied()
+        .and_then(|x| x)
+        .ok_or_else(|| Error::Decode(character.into()))
+}
+
+#[inline]
+fn uncorrected_offset(character: &str) -> i32 {
+    let mut offset = 0;
+    let mut increment = 32;
+
+    character.bytes().for_each(|u| {
+        match u {
+            b'.' => offset += increment,
+            b'-' => offset -= increment,
+            _ => (),
+        }
+        increment /= 2;
+    });
+
+    offset
 }
 
 #[cfg(test)]
@@ -178,7 +304,7 @@ mod tests {
     #[test]
     fn char_to_code_works() {
         let sequence = "abcdefghijklmnopqrstuvwxyz0123456789";
-        let pairs = sequence.bytes().zip(super::CODE);
+        let pairs = sequence.bytes().zip(super::data::ENCODED_SEQUENCES);
 
         for (u, &code) in pairs {
             assert_eq!(super::encode_byte(u).unwrap(), code);
