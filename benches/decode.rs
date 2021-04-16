1654df6c5ae588c43366a403258b3b44c9a857f5
J/A <archer884@gmail.com>
2021-04-16 00:47:01 -0500

    Benchmark heap code

--- benches/decode.rs
+++ benches/decode.rs
@@ -160,6 +160,72 @@ mod data {
         None,
         Some(b'5'),
     ];
+
+    pub static DECODING_ARRAY_2: &[Option<u8>] = &[
+        None,
+        Some(b'E'),
+        Some(b'T'),
+        Some(b'I'),
+        Some(b'A'),
+        Some(b'N'),
+        Some(b'M'),
+        Some(b'S'),
+        Some(b'U'),
+        Some(b'R'),
+        Some(b'W'),
+        Some(b'D'),
+        Some(b'K'),
+        Some(b'G'),
+        Some(b'O'),
+        Some(b'H'),
+        Some(b'V'),
+        Some(b'F'),
+        None,
+        Some(b'L'),
+        None,
+        Some(b'P'),
+        Some(b'J'),
+        Some(b'B'),
+        Some(b'X'),
+        Some(b'C'),
+        Some(b'Y'),
+        Some(b'Z'),
+        Some(b'Q'),
+        None,
+        None,
+        Some(b'5'),
+        Some(b'4'),
+        None,
+        Some(b'3'),
+        None,
+        None,
+        None,
+        Some(b'2'),
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        Some(b'1'),
+        Some(b'6'),
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        Some(b'7'),
+        None,
+        None,
+        None,
+        Some(b'8'),
+        None,
+        Some(b'9'),
+        Some(b'0'),
+    ];
 }
 
 /// The original decoding method involved a hashmap.
@@ -224,6 +290,25 @@ fn uncorrected_offset(character: &str) -> i32 {
     offset
 }
 
+#[inline]
+fn decode_character_heap(character: &str) -> Result<u8> {
+    let idx = character_index(character);
+    data::DECODING_ARRAY_2
+        .get(idx as usize)
+        .copied()
+        .and_then(|x| x)
+        .ok_or_else(|| Error::Decode(character.into()))
+}
+
+#[inline]
+fn character_index(character: &str) -> i32 {
+    character.bytes().fold(0, |idx, u| match u {
+        b'.' => idx * 2 + 1,
+        b'-' => idx * 2 + 2,
+        _ => idx,
+    })
+}
+
 fn criterion_benchmark(c: &mut Criterion) {
     let decoder = CharacterDecoder::new();
     let sequences: Vec<_> = data::ENCODED_SEQUENCES
@@ -245,6 +330,14 @@ fn criterion_benchmark(c: &mut Criterion) {
         b.iter(|| {
             for &character in &sequences {
                 black_box(decode_character(character).unwrap());
+            }
+        })
+    });
+
+    c.bench_function("ftree 2.0", |b| {
+        b.iter(|| {
+            for &character in &sequences {
+                black_box(decode_character_heap(character).unwrap());
             }
         })
     });
